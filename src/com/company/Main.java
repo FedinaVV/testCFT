196c332157a3ec6e95307721352a73f26bf7aaf9
FedinaVV <lerocha_2009@mail.ru>
2024-01-15 23:59:02 +0700

    Добавление опции -s с краткой статистикой по файлам

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -19,6 +16,9 @@ public class Main {
         String pathToSaveFile = "";
         String prefixOutFiles = "";
         List<String> options = new ArrayList<String>(Arrays.asList(args));
+        int countInt = 0;
+        int countFl = 0;
+        int countSt = 0;
 
         if (options.contains("-o") || options.contains("-о")) {
             int index = options.indexOf("-o");
@@ -49,10 +49,10 @@ public class Main {
             prefixOutFiles = options.get(index + 1);
 
             if (prefixOutFiles.contains("\\") || prefixOutFiles.contains("/") || prefixOutFiles.contains(":") || prefixOutFiles.contains("*")
-                || prefixOutFiles.contains("?") || prefixOutFiles.contains("\"") || prefixOutFiles.contains("<") || prefixOutFiles.contains(">")
+                    || prefixOutFiles.contains("?") || prefixOutFiles.contains("\"") || prefixOutFiles.contains("<") || prefixOutFiles.contains(">")
                     || prefixOutFiles.contains("|") || prefixOutFiles.contains("+")) {
                 System.out.println("Символы \\ / : * ? \" < > | + не допускаются для использования в именах файлов");
-                throw new IOException ("Недопустимые символы в имени файлов");
+                throw new IOException("Недопустимые символы в имени файлов");
             }
         }
 
@@ -61,8 +61,8 @@ public class Main {
         if (!options.contains("-а") && !options.contains("-a")) {
 
             File oldFileString = new File(prefixOutFiles + "strings.txt");
-            File oldFileInt = new File(prefixOutFiles +"integers.txt");
-            File oldFileFloat = new File(prefixOutFiles +"floats.txt");
+            File oldFileInt = new File(prefixOutFiles + "integers.txt");
+            File oldFileFloat = new File(prefixOutFiles + "floats.txt");
             oldFileString.delete();
             oldFileInt.delete();
             oldFileFloat.delete();
@@ -102,6 +102,11 @@ public class Main {
                     writerInt.write("\n");
                 }
                 writerInt.close();
+
+                if (options.contains("-s")) {
+                    countInt = getLinesCount(pathToSaveFile + prefixOutFiles + "integers.txt");
+                    System.out.println("Количество строк в файле integers: " + countInt);
+                }
             }
 
             if (listOfFloat.size() > 0) {
@@ -110,9 +115,15 @@ public class Main {
                 for (Float item : listOfFloat) {
                     writerFloat.write(item.toString());
                     writerFloat.write("\n");
+
                 }
 
                 writerFloat.close();
+
+                if (options.contains("-s")) {
+                    countFl = getLinesCount(pathToSaveFile + prefixOutFiles + "floats.txt");
+                    System.out.println("Количество строк в файле floats: " + countFl);
+                }
             }
 
             if (listOfString.size() > 0) {
@@ -121,9 +132,15 @@ public class Main {
                 for (String item : listOfString) {
                     writerString.write(item);
                     writerString.write("\n");
+
                 }
 
                 writerString.close();
+
+                if (options.contains("-s")) {
+                    countSt = getLinesCount(pathToSaveFile + prefixOutFiles + "strings.txt");
+                    System.out.println("Количество строк в файле strings: " + countSt);
+                }
             }
 
             scanner.close();
@@ -132,4 +149,21 @@ public class Main {
             System.out.println("Возникла ошибка во время записи, проверьте данные.");
         }
     }
+
+    public static Integer getLinesCount(String path) {
+
+        int count = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                count++;
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при считывании строк в файле");
+        }
+
+        return count;
+    }
+
 }
